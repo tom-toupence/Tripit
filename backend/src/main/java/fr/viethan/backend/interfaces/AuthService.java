31fb6ceccc0c60317a0aae6a12f22ab4a46e6d69
unknown <tom06530@gmail.com>
2025-05-14 05:47:28 +0200

    arbo refactor

--- backend/src/main/java/fr/viethan/backend/interfaces/AuthService.java
+++
@@ -1,9 +0,0 @@
-package fr.viethan.backend.interfaces;
-
-import fr.viethan.backend.dto.AuthDTO;
-import fr.viethan.backend.dto.AuthResponseDTO;
-
-public interface AuthService {
-    AuthResponseDTO authenticate(AuthDTO authDTO);
-    AuthResponseDTO register(AuthDTO authDTO);
-}
--- backend/src/main/java/fr/viethan/backend/interfaces/StepService.java
+++
@@ -1,8 +0,0 @@
-package fr.viethan.backend.interfaces;
-
-import fr.viethan.backend.dto.StepDTO;
-import java.util.List;
-
-public interface StepService {
-    List<StepDTO> getStepsByTripId(Long tripId);
-}

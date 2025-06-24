dc06c904472d9f56c30a1f6ba2142263dd3bbaa2
Emma Allain <emma.allain@etu.univ-cotedazur.fr>
2025-06-24 07:22:56 +0200

    création trip

---
+++ backend/set-mail-env.bat
@@ -0,0 +1,11 @@
+@echo off
+set GOOGLE_CLIENT_ID=827947979043-v4e1s9k55f1jv99d2f093n2kac055m3l.apps.googleusercontent.com
+set GOOGLE_CLIENT_SECRET=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
+set JWT_SECRET=JtIhQdeNlGWqWXhnKD+DqMaJzgP/HawNYQdrlUvhlKkRlpSZ1YSbIOeHKxrep8xX/DfZ/OTrdFDjJZqDsSO+ww==
+set MAIL_HOST=smtp.gmail.com
+set MAIL_PORT=587
+set MAIL_USERNAME=tripit@gmail.com
+set MAIL_PASSWORD=cm8o*@wpQa%Wi#5HvJ&y
+set MAIL_FROM=tripit@gmail.com
+set CONTACT_RECEIVER=toupencetom@gmail.com
+echo Environment variables set for this CMD session !
